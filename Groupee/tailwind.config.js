cc34a5220f7fe1721a74c67cbbc870de68dd4bfb
Andrea Micheli <amicheli@kth.se>
2024-09-27 13:28:53 +0200

    fixed firebase setting for build and some minor fixes in landing page

--- Groupee/tailwind.config.js
+++ Groupee/tailwind.config.js
@@ -9,8 +9,12 @@ module.exports = {
       "t-light": "#FAF0E8",
       "t-dark": "#1F4545",
       "t-orange": "#FF6433",
+      "t-orange-hover": "#FF4D00",
+      "t-orange-active": "#992400", // 50% brightness of #FF6433
       "t-blue": "#00FFFF",
       "t-green": "#00FFC2",
+      "t-green-hover": "#00E88F",
+      "t-green-active": "#008062",
     },
     fontFamily: {
       poppins: ["Poppins", "sans-serif"],
