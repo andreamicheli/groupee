0580401b39dd20cec28ba112517e2a9c9156a31e
Andrea Micheli <amicheli@kth.se>
2024-10-01 14:35:41 +0300

    added navbar compo

--- Groupee/src/app/app.component.ts
+++ Groupee/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatIconModule],
+  imports: [RouterOutlet, MatIconModule, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
