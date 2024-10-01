81632c61b3aa398047a9cbf67e0e69d8c6f5ec40
Andrea Micheli <amicheli@kth.se>
2024-10-01 14:47:55 +0300

    added landing component

--- Groupee/src/app/app.component.ts
+++ Groupee/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { LandingComponent } from './pages/landing/landing.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatIconModule, NavbarComponent],
+  imports: [RouterOutlet, MatIconModule, NavbarComponent, LandingComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
---
+++ Groupee/src/app/pages/landing/landing.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-landing',
+  standalone: true,
+  imports: [MatIconModule],
+  templateUrl: './landing.component.html',
+  styleUrl: './landing.component.css',
+})
+export class LandingComponent {}
