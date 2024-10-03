66f47cfb3ec0ad78292443ffdc3d1d81cf3c283d
Andrea Micheli <amicheli@kth.se>
2024-10-03 16:54:25 +0300

    added basic routing

--- Groupee/src/app/app.routes.ts
+++ Groupee/src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { LandingComponent } from './pages/landing/landing.component';
+import { ClientCodeComponent } from './pages/client/code/code.component';
+import { ClientWaitingComponent } from './pages/client/waiting/waiting.component';
+import { HostSettingsComponent } from './pages/host/settings/settings.component';
+import { HostWaitingComponent } from './pages/host/waiting/waiting.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'landing', component: LandingComponent },
+  { path: 'client/code', component: ClientCodeComponent },
+  { path: 'client/waiting', component: ClientWaitingComponent },
+  { path: 'host/settings', component: HostSettingsComponent },
+  { path: 'host/waiting', component: HostWaitingComponent },
+  { path: '', redirectTo: '/landing', pathMatch: 'full' },
+  { path: '**', redirectTo: '/landing' }, // Wildcard route for a 404 page
+];
---
+++ Groupee/src/app/pages/client/waiting/waiting.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-client-waiting',
+  standalone: true,
+  imports: [],
+  templateUrl: './waiting.component.html',
+  styleUrl: './waiting.component.css',
+})
+export class ClientWaitingComponent {}
